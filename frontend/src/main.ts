0bde93c98b9eb5ba82a8b93724284fbddbfc93b8
BlueSkyFish <blueskyfish@blueskyfish.de>
2018-08-05 14:27:35 +0200

    fixed the wrong declaration

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,8 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, NgModuleRef } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { NgModuleRef } from '../node_modules/@angular/core/src/render3';
 
 if (environment.production) {
   enableProdMode();
